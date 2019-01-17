4e818d0e03fadfd1245e89c2eee54689661b8315
tanderson53 <42656935+tanderson53@users.noreply.github.com>
2019-01-17 09:54:21 -0600

    Update hello.c

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+/*
+ * Author: Tyler Anderson
+ * Hack 1
+ * This program prints my name, major
+ */
+
+int main(int argc, char **argv) {
+  printf("Tyler Mark Anderson\n");
+  printf("Computer Science\n");
+
+  return 0;
+}
