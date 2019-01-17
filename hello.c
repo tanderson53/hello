480054a95268377deb52ce99d07dd7c59030d094
rting2 <46720099+rting2@users.noreply.github.com>
2019-01-17 10:07:04 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -10,6 +10,8 @@
 int main(int argc, char **argv) {
   printf("Tyler Mark Anderson\n");
   printf("Computer Science\n");
+  printf("Rong Xiang Ting\n");
+  printf("Computer Science\n");
 
   return 0;
 }
